381ba1a47be26d1f6a34c8200d5c39717b8002a6
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-16 12:00:42 -0300

    Consulta com filtros

--- src/main/java/br/com/vinicius/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/vinicius/forum/repository/TopicoRepository.java
@@ -1,9 +1,13 @@
 package br.com.vinicius.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.vinicius.forum.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCursoNome(String nomeCurso);
 
 }
