9f63c614c3729eac3c21555ad21747ee989112c3
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-17 09:41:52 -0300

    Validações com Bean Validation

--- src/main/java/br/com/vinicius/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/vinicius/forum/controller/form/TopicoForm.java
@@ -1,13 +1,24 @@
 package br.com.vinicius.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.sun.istack.NotNull;
+
 import br.com.vinicius.forum.model.Curso;
 import br.com.vinicius.forum.model.Topico;
 import br.com.vinicius.forum.repository.CursoRepository;
 
 public class TopicoForm {
 
+	@NotNull @NotEmpty @Length(min = 5)
 	private String titulo;
+	
+	@NotNull @NotEmpty @Length(min = 10)
 	private String mensagem;
+	
+	@NotNull @NotEmpty
 	private String nomeCurso;
 
 	public String getTitulo() {
