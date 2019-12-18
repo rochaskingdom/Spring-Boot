bf92c7b735e497d8951e6905f00658ce7071c627
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-18 08:49:37 -0300

    Paginação

--- src/main/java/br/com/vinicius/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/vinicius/forum/repository/TopicoRepository.java
@@ -1,13 +1,13 @@
 package br.com.vinicius.forum.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.vinicius.forum.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-	List<Topico> findByCursoNome(String nomeCurso);
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 
 }
