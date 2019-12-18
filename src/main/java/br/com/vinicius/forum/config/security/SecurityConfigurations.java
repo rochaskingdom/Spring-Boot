a1f3181f9fa0c3673ef7909417d50d9a131bb1cb
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-18 13:10:03 -0300

    Configurando autenticação Stateless

--- src/main/java/br/com/vinicius/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/vinicius/forum/config/security/SecurityConfigurations.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -26,8 +27,13 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	// Configuracoes de Autorizacao
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
-				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll().anyRequest().authenticated().and().formLogin();
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		.antMatchers(HttpMethod.POST, "/auth").permitAll()
+		.anyRequest().authenticated()
+		.and().csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
 	// Configuracoes de recursos estaticos(js, css, imagens, etc.)
