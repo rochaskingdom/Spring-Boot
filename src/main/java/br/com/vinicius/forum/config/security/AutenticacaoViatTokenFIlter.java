d58722191911107ab9fe1d0cf0ff9e515ba72289
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-19 11:03:02 -0300

    Autenticando o cliente via Spring Security

--- src/main/java/br/com/vinicius/forum/config/security/AutenticacaoViatTokenFIlter.java
+++ src/main/java/br/com/vinicius/forum/config/security/AutenticacaoViatTokenFIlter.java
@@ -7,25 +7,41 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.vinicius.forum.model.Usuario;
+import br.com.vinicius.forum.repository.UsuarioRepository;
 
 public class AutenticacaoViatTokenFIlter extends OncePerRequestFilter {
 
 	private TokenService tokenService;
-	
-	public AutenticacaoViatTokenFIlter(TokenService tokenService) {
+	private UsuarioRepository repository;
+
+	public AutenticacaoViatTokenFIlter(TokenService tokenService, UsuarioRepository repository) {
 		this.tokenService = tokenService;
 	}
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		
+
 		String token = recuperarToken(request);
 		boolean valido = tokenService.isTokenValido(token);
-		System.out.println(valido);
-		
+		if (valido) {
+			autenticarCliente(token);
+		}
+
 		filterChain.doFilter(request, response);
+	}
+
+	private void autenticarCliente(String token) {
+		Long idUsuario = tokenService.getIdUsuario(token);
+		Usuario usuario = repository.findById(idUsuario).get();
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null,
+				usuario.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
 	}
 
 	private String recuperarToken(HttpServletRequest request) {
@@ -33,7 +49,7 @@ public class AutenticacaoViatTokenFIlter extends OncePerRequestFilter {
 		if (token == null || token.isEmpty() || !token.startsWith("Bearer")) {
 			return null;
 		}
-		
+
 		return token.substring(7, token.length());
 	}
 
