0399f0b2c7e29d1d5062e8b2de1422c1a9b4c3e5
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-23 09:22:37 -0300

    Conhecendo Spring Boot Admin

---
+++ src/test/java/br/com/vinicius/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.springbootadmin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
