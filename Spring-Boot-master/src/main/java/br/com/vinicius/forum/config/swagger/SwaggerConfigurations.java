7f82de8663b494289470f43896a570daf6671c96
Vinicius Rodrigues Rocha <44781974+rochaskingdom@users.noreply.github.com>
2019-12-23 11:30:29 -0300

    Documentação da API com SpringFox Swagger

---
+++ Spring-Boot-master/src/main/java/br/com/vinicius/forum/config/swagger/SwaggerConfigurations.java
@@ -0,0 +1,24 @@
+package br.com.vinicius.forum.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.vinicius.forum.model.Usuario;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfigurations {
+
+	@Bean
+	public Docket api() {
+	return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("br.com.vinicius.forum"))
+            .paths(PathSelectors.ant("/**"))
+            .build()
+            .ignoredParameterTypes(Usuario.class);
+	}
+}
